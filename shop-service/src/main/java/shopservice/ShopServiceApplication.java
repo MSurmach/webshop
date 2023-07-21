fc09dbd06b7fac1be7b565c759131c93ed8c2d16
Maxim Surmach <maximsurmach@gmail.com>
2023-07-21 14:54:37 +0300

    Init all services as spring boot applications

---
+++ shop-service/src/main/java/shopservice/ShopServiceApplication.java
@@ -0,0 +1,11 @@
+package shopservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShopServiceApplication.class, args);
+    }
+}
