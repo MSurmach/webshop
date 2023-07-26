903b0349be4ca0380762d72c7b6a3093d3eacebe
Maxim Surmach <maximsurmach@gmail.com>
2023-07-26 14:04:13 +0300

    Add order-service rabbitmq backbone

--- order-service/src/main/java/com/intexsoft/webshop/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/intexsoft/webshop/orderservice/OrderServiceApplication.java
@@ -2,9 +2,11 @@ package com.intexsoft.webshop.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class OrderServiceApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
     }
---
+++ order-service/src/main/java/com/intexsoft/webshop/orderservice/config/RabbitMQConfig.java
@@ -0,0 +1,44 @@
+package com.intexsoft.webshop.orderservice.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    @Value("${rmq.event.exchange}")
+    private String eventExchangeName;
+    @Value("${rmq.command.queue}")
+    private String commandQueueName;
+    @Value("${rmq.command.exchange}")
+    private String commandExchangeName;
+    @Value("${rmq.command.routing")
+    private String commandRouting;
+
+    @Bean
+    public TopicExchange eventExchange() {
+        return new TopicExchange(eventExchangeName);
+    }
+
+    @Bean
+    public Queue commandQueue() {
+        return new Queue(commandQueueName);
+    }
+
+    @Bean
+    public TopicExchange commandExchange() {
+        return new TopicExchange(commandExchangeName);
+    }
+
+    @Bean
+    public Binding commandBinding() {
+        return BindingBuilder
+                .bind(commandQueue())
+                .to(commandExchange())
+                .with(commandRouting);
+    }
+}
